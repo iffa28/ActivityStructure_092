bc32e29e07fb7805e2f3068f461cdf379473a63a
iffa28 <127277497+iffa28@users.noreply.github.com>
2023-04-05 11:11:06 +0700

    menggunakan ignore agar syntax nya di abaikan

--- Activity02/Activity02.cpp
+++ Activity02/Activity02.cpp
@@ -20,7 +20,7 @@ int main() {
 
 	cout << "Masukkan NIM : ";  
 	cin.getline(mhs.nim,12);
-	cout << "Masukkan Umur : ";
+	cout << "Masukkan Umur : "; 
 	cin >> mhs.umur;
 	cin.ignore(1, '\n');
 	cout << "Masukkan Nama : ";
