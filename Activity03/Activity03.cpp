d0ba493bda732be0acb490601790ff7917ba196b
iffa28 <127277497+iffa28@users.noreply.github.com>
2023-04-05 11:27:38 +0700

    syntax untuk melakukan perulangan

---
+++ Activity03/Activity03.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+	char desa[20];
+	char kota[20];
+};
+
+struct Mahasiswa
+{
+	char nim[12];
+	char nama[20];
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << "Masukkan NIM : ";
+		cin.getline(mhs[i].nim, 12);
+		cout << "Masukkan Umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << "Masukkan Nama : ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat : ";
+		cout << "\n\tMasukkan Desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\tMasukkan Kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		cout << "\n\nNIM : " << mhs[i].nim;
+		cout << "\nUmur : " << mhs[i].umur;
+		cout << "\nNama : " << mhs[i].nama;
+		cout << "\nAlamat : ";
+		cout << "\n\tDesa : " << mhs[i].alamat.desa;
+		cout << "\n\tkota : " << mhs[i].alamat.kota;
+	}
+}
